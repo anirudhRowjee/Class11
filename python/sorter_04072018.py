06979fd44191a8f52056b2235801715af1c88b62
tanwer-pradeep <63426820+tanwer-pradeep@users.noreply.github.com>
2020-10-01 15:32:27 +0530

    improved with loop
    changed the input method, now its using loop and hence can also change for n inputs.
    and also change the output print statement as we are using the list.

--- python/sorter_04072018.py
+++ python/sorter_04072018.py
@@ -1,22 +1,27 @@
 
-##one = input("number 1 \n")
-##two = input("number 2 \n")
-##three = input("number 3 \n")
-##
-##x = []
-##
-##x.append(one)
-##x.append(two)
-##x.append(three)
-##
-##x.sort()
-##y = len(x)
-##
-##print "smallest number is " + str(x[0])
-##print "largest is " + str(x[y -1])
+"""
+i used for loop for taking multiple values.
+one = input("number 1 \n")
+two = input("number 2 \n")
+three = input("number 3 \n")
 
-x = list(input("enter a list of numbers, separated by commas"))
+x = [] (intialies this empty list in the starting)
+
+x.append(one)
+x.append(two)
+x.append(three)
+
 x.sort()
-y = len(x)
+y = len(x) (no need of this  as we can use negative indexing)
+
 print "smallest number is " + str(x[0])
-print "largest is " + str(x[y - 1])
+print "largest is " + str(x[y -1])"""
+
+x = []
+for i in range(0, 3):
+    y = int(input("enter three numbers each followed by an 'Enter'"))
+    x.append(y)
+x.sort()
+
+print("smallest number is ", x[0])
+print("largest is ", x[-1])
